10708eaed36946f9956eee62e1e61f7c62c59094
reo0306 <reo19840306@gmail.com>
2024-12-05 07:34:07 +0900

    update router etc

--- rust-book-manager/registry/src/lib.rs
+++ rust-book-manager/registry/src/lib.rs
@@ -1,26 +1,42 @@
 use std::sync::Arc;
 
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
-use kernel::repository::health::HealthCheckRepository;
+use adapter::{
+    database::ConnectionPool,
+    repository::{
+        book::BookRepositoryImpl,
+        health::HealthCheckRepositoryImpl,
+    }
+};
+use kernel::repository::{
+    health::HealthCheckRepository,
+    book::BookRepository,
+};
 
 // 1) DIコンテナの役割を果たす構造体を定義する。Cloneはのちほどaxum側で必要になるため
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
+    book_repository: Arc<dyn BookRepository>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         // 2) 依存解決を行う。関数内で手書きする。
         let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        let book_repository = Arc::new(BookRepositoryImpl::new(pool.clone()));
 
         Self {
             health_check_repository,
+            book_repository,
         }
     }
 
     // 3) 依存解決したインスタンスを返すメソッドを定義する
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.clone()
+    }
+
+    pub fn book_repository(&self) -> Arc<dyn BookRepository> {
+        self.book_repository.clone()
     }
 }
--- rust-book-manager/src/bin/app.rs
+++ rust-book-manager/src/bin/app.rs
@@ -2,7 +2,10 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
 use anyhow::{Error, Result};
-use api::route::health::build_healtth_check_routers;
+use api::route::{
+    health::build_healtth_check_routers,
+    book::build_book_routers,
+};
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
@@ -26,6 +29,7 @@ async fn bootstrap() -> Result<()> {
     // 5) build_health_check_routers関数を呼び出す。AppRegistryをRouterに登録しておく
     let app = Router::new()
         .merge(build_healtth_check_routers())
+        .merge(build_book_routers())
         .with_state(registry);
 
     // 6) サーバーを起動する
